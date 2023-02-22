70b148c0a9e2b27b477454808a974a2d0c3261ba
sattarawut <panpon9988@gmail.com>
2023-02-22 11:36:12 +0700

    add data in ignore

--- data_templates/layout.py
+++ data_templates/layout.py
@@ -2,20 +2,19 @@ import dash_bootstrap_components as dbc
 from dash import Dash, html
 from data import *
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
+app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
 
 app.layout = html.Div(
     children=[
         html.Div(
             children=[
                 html.H1(
-                    children="Statistics of Road Traffic Fatalities in 2554-2565",
+                    children="Mechine Learning For Prediction Grade",
                     style={
                         "textAlign": "center",
                     },
                 )
             ],
-            className="row",
         ),
     ],
 )
