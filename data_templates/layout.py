3ccbd6c19b3c4c7a8bf7c6c251c215529231c49f
sattarawut <panpon9988@gmail.com>
2023-02-26 22:55:24 +0700

    add page layout full screen

--- data_templates/layout.py
+++ data_templates/layout.py
@@ -9,12 +9,23 @@ app.layout = html.Div(
         html.Div(
             children=[
                 html.H1(
+                    children="Mechine Learning",
+                    style={
+                        "left" : "0vw" , "padding-left" : "3vh","color" : "#F8F8FF" ,"margin": "0","height":"13vh"
+                    },
+                )
+           
+            ], style={"background-color" : "#000000"},
+        ),
+        html.Div(
+            children=[
+                html.H1(
                     children="Mechine Learning For Prediction Grade",
                     style={
-                        "textAlign": "center",
+                        "textAlign": "center","background-color" : "#D3D3D3","margin": "0 5vw","bottom": "0px","height":"87vh"
                     },
                 )
             ],
         ),
-    ],
+    ],style={"height":"100vh"}
 )
